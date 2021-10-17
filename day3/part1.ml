6d8f47202e599286ffefef9e6b278452976b4a11
Élise Rubio <elise.rubio@4sh.fr>
2021-10-17 19:01:53 +0200

    :sparkles: Day 3 part 1 answer

--- day3/part1.ml
+++ day3/part1.ml
@@ -1,11 +1,38 @@
 open Printf
+open Hashtbl
 
 let input = "input.txt"
 
-let rec input_lines file =
-   match try (input_line file) with End_of_file -> "" with
-      | "" -> 0
-      | line -> (input_lines file)
-    ;;
+let rec string_to_char_list ch = match ch with
+    | "" -> []
+    | ch -> (String.get ch 0 ) :: (string_to_char_list (String.sub ch 1 ( (String.length ch)-1) ) )  ;;
 
-print_int (input_lines (open_in input));;
+let move direction (x, y) = match direction with
+    | 'v' -> (x, y - 1)
+    | '^' -> (x, y + 1)
+    | '>' -> (x + 1, y)
+    | _ ->  (x - 1, y)
+;;
+
+let deliverPresent houses position =
+    if (Hashtbl.mem houses position) then
+            (Hashtbl.replace houses position ((Hashtbl.find houses position) + 1))
+        else
+            (Hashtbl.add houses position 1)
+;;
+
+let rec deliverPresents instructions houses currentPosition = match instructions with
+    | [] -> ()
+    | h::t ->
+    let newPosition = (move h currentPosition) in
+    (deliverPresent houses newPosition);
+    (deliverPresents t houses newPosition)
+;;
+
+let instructions = (string_to_char_list (input_line (open_in input)));;
+let houses = (Hashtbl.create 1);;
+(Hashtbl.add houses (0, 0) 1);;
+
+(deliverPresents instructions houses (0, 0));;
+
+print_int (Hashtbl.length houses);;
